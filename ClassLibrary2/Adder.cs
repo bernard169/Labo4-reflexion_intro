d7222a67808ac634dbe48b4eb1d6f0022d24ea47
bernard169 <bernard@tourneur.org>
2018-11-09 17:03:43 +0100

    first commit

---
+++ ClassLibrary2/Adder.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using computer;
+namespace Adder
+{
+    public class Adder : Computer
+    {
+        public string Name
+        {
+            get { return "Adder"; }
+        }
+
+        public double Compute(params double[] values)
+        {
+            double result = 0;
+            Array.ForEach(values, value => result += value);
+            return result;
+        }
+    }
+}
---
+++ introspection.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Reflection;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labo_4
+{
+    public class Item
+    {
+        private static int nextid = 0;
+
+        public readonly int ID;
+        private readonly string name;
+        private readonly int price;
+
+        public Item(string name, int price)
+        {
+            this.ID = ++nextid;
+            this.name = name;
+            this.price = price;
+        }
+
+        public string Name
+        {
+            get { return name; }
+        }
+
+        public int Price
+        {
+            get { return price; }
+        }
+    }
+    public class Introspection
+    {
+        public readonly List<FieldInfo> attributs;
+        public readonly List<MethodInfo> methods;
+        private readonly Type type;
+        public Introspection(Type type)
+        {
+            this.type = type;
+            attributs = new List<FieldInfo>();
+            methods = new List<MethodInfo>();
+            foreach (FieldInfo info in type.GetFields (BindingFlags.NonPublic | BindingFlags.Static))
+            {
+                attributs.Add(info);
+            }
+            foreach (MethodInfo info in type.GetMethods (BindingFlags.NonPublic | BindingFlags.Static))
+            {
+                methods.Add(info);
+            }
+        }
+        public override string ToString()
+        {
+            return String.Format(("Type : {0}" +
+                "\n Attributs : {1}" +
+                "\n Methods : {2}"), type, attributs, methods);
+        }
+    }
+}
